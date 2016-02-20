0d4c19c45c0ffe001f57ccc9e9577f8c14213db0
francishuang <francis3925@gmail.com>
2016-02-20 23:46:09 +0800

    group#index post DESC

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -3,6 +3,6 @@ class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = current_user.participated_groups
+    @groups = current_user.participated_groups('post_count DESC')
   end
 end
